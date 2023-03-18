ad612bc47432d567a4713c849999fb0d6231f416
rovshan <rouvsen@gmail.com>
2023-03-19 01:09:06 +0400

    MySql ==> H2DB

---
+++ src/main/java/com/rouvsen/Helper.java
@@ -0,0 +1,7 @@
+package com.rouvsen;
+
+public class Helper {
+    public static void main(String[] args) {
+        System.out.println("ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';".toLowerCase());
+    }
+}
